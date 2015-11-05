dfd5c180a8d6177565fbd02a9a83236823c59687
Karol Sochacki <k.sochcki@users.github.com>
2015-11-05 21:37:49 +0100

    commit drugi

--- src/com/example/hellowworld/HellowWorld.java
+++ src/com/example/hellowworld/HellowWorld.java
@@ -5,7 +5,13 @@ package com.example.hellowworld;
  */
 public class HellowWorld {
     public static void main(String[] args) {
-        System.out.println("Hellow World");
+        System.out.println("Hellow ." +
+                "World");
+
+
     }
-    
 }
+
+
+
+
