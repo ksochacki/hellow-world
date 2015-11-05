1781687bb0578895cfb505a4cdaa40a39efcccc6
Karol Sochacki <k.sochcki@users.github.com>
2015-11-05 21:00:16 +0100

    pierwszy commit

---
+++ src/com/example/hellowworld/HellowWorld.java
@@ -0,0 +1,11 @@
+package com.example.hellowworld;
+
+/**
+ * Created by USER on 2015-11-01.
+ */
+public class HellowWorld {
+    public static void main(String[] args) {
+        System.out.println("Hellow World");
+    }
+    
+}
